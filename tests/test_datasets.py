46518b4706c19ee55ce16f435a7a822fd37ff630
Eliot Berriot <contact@hat.eliotberriot.com>
2015-06-17 09:32:48 +0200

    Splitted tests in multiple files

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,58 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-test_django-modelstats
-------------
-
-Tests for `django-modelstats` models module.
-"""
-
-import os
-import shutil
 import datetime
 
-from django.test import TestCase
-from django.contrib.auth import get_user_model
+from .base import TestBase
 
 from modelstats import models
 from modelstats import datasets
 
 
-class TestModelstats(TestCase):
-
-    def setUp(self):
-        self.user_model = get_user_model()
-        self.default_dates_joined = [
-            (datetime.datetime(2015, 1, 1, 23, 12), 11),
-            (datetime.datetime(2015, 1, 2, 23, 12), 8),
-            (datetime.datetime(2015, 1, 3, 23, 12), 7),
-            (datetime.datetime(2015, 1, 4, 23, 12), 15),
-            (datetime.datetime(2015, 1, 5, 23, 12), 8),
-            (datetime.datetime(2015, 1, 6, 23, 12), 2),
-            (datetime.datetime(2015, 1, 7, 23, 12), 19),
-            (datetime.datetime(2015, 1, 8, 23, 12), 20),
-            (datetime.datetime(2015, 1, 10, 23, 12), 25),
-            (datetime.datetime(2015, 1, 11, 23, 12), 3),
-        ]
+class TestDateDataSet(TestBase):
 
 
-
-    def create_users(self, dates_joined=None):
-        dates_joined = dates_joined or self.default_dates_joined
-        users = []
-
-        for i, data in enumerate(dates_joined):
-            date, quantity = data
-            q = 0
-            while q < quantity:
-                username = str(i) + str(q)
-                u = self.user_model(date_joined=date, username=username)
-                u.save()
-                self.assertEqual(u.date_joined, date)
-                users.append(u)
-                q += 1
-        return users
     def test_datedataset(self):
         self.create_users()
         queryset = self.user_model.objects.all()
@@ -84,12 +40,23 @@ class TestModelstats(TestCase):
         self.assertEqual(dataset.data[0]['total'], total_quantity)
 
     def test_datedataset_fill_missing_dates_day(self):
-        self.create_users()
+        dates_joined = [
+            (datetime.datetime(2015, 1, 1, 23, 12), 11),
+            (datetime.datetime(2015, 1, 2, 23, 12), 8),
+            (datetime.datetime(2015, 1, 5, 23, 12), 7),
+        ]
+
+        users = self.create_users(dates_joined)
+
         queryset = self.user_model.objects.all()
         dataset = datasets.DateDataSet(datetime_field='date_joined', fill_missing_dates=True, queryset=queryset).process()
 
-        self.assertEqual(dataset.data[8]['day'], '2015-01-09')
-        self.assertEqual(dataset.data[8]['total'], 0)
+        self.assertEqual(dataset.data[0]['day'], '2015-01-01')
+        self.assertEqual(dataset.data[1]['day'], '2015-01-02')
+        self.assertEqual(dataset.data[2]['day'], '2015-01-03')
+        self.assertEqual(dataset.data[3]['day'], '2015-01-04')
+        self.assertEqual(dataset.data[4]['day'], '2015-01-05')
+        self.assertEqual(dataset.data[2]['total'], 0)
 
 
     def test_datedataset_fill_missing_dates_month(self):
